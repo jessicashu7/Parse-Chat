0f65959bc42e891e37943cd40558d4a0b3d7ed95
Jessica Shu <jessicashu7@fb.com>
2018-07-08 23:19:52 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Jessica Shu on 7/8/18.
+//  Copyright © 2018 jessicashu7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
